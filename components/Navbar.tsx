42778a84d644dfbf607cd9111d135218a842d55a
Rajan Pandey <rajanpandeygiit31078379@gmail.com>
2024-08-21 10:32:40 +0530

    hamburger button updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,17 @@
+"use client"
 import { NAV_LINKS } from "../constants/index"
 import Image from "next/image"
 import Link from "next/link"
 import Button from "./Button"
+import { useState } from "react"
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
@@ -33,7 +41,23 @@ const Navbar = () => {
         width={32}
         height={32}
         className="inline-block cursor-pointer lg:hidden"
+        onClick={toggleMenu}
       />
+      {isMenuOpen && (
+        <ul className="absolute top-full left-0 w-full bg-white p-4 flex flex-col items-center lg:hidden">
+          {NAV_LINKS.map((link) => (
+            <Link href={link.href} key={link.key} className="regular-16 text-gray-50 py-2 cursor-pointer transition-all hover:font-bold">
+              {link.label}
+            </Link>
+          ))}
+          <Button 
+            type="button"
+            title="Login"
+            icon="/user.svg"
+            variant="btn_dark_green"
+          />
+        </ul>
+      )}
     </nav>
   )
 }
